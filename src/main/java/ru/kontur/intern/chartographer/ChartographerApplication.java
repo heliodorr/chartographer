42f0f4f4cee640ccf922e3466e7004c219129685
heliodorr <awake@MacBook-Pro-Maksim.local>
2022-03-20 18:33:26 +0300

    cleaning

--- src/main/java/ru/kontur/intern/chartographer/ChartographerApplication.java
+++ src/main/java/ru/kontur/intern/chartographer/ChartographerApplication.java
@@ -1,11 +1,8 @@
 package ru.kontur.intern.chartographer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class ChartographerApplication {
 
