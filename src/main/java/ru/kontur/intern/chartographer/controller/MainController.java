db6b39cc451fc45c583fc47422fb46b4a6f55018
heliodorr <98260994+heliodorr@users.noreply.github.com>
2022-03-29 15:43:12 +0300

    Update MainController.java
    cleaning

--- src/main/java/ru/kontur/intern/chartographer/controller/MainController.java
+++ src/main/java/ru/kontur/intern/chartographer/controller/MainController.java
@@ -83,11 +83,6 @@ public class MainController {
 
   }
 
-//  @DeleteMapping("/chartas/{id}/")
-//  public void deleteRedirect(HttpServletResponse response) throws IOException {
-//    response.sendRedirect("/chartas/{id}/");
-//  }
-
 }
 
 
